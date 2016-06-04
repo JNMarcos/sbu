1494f01c1dd315fdaf0770376ee96f93e3996968
Marcos Inacio de Paula Lima <marcos.inacio.recife@hotmail.com>
2016-06-03 22:40:04 -0300

    Classe Basica Usuario feita

--- src/classes_basicas/Usuario.java
+++ src/classes_basicas/Usuario.java
@@ -6,7 +6,11 @@ import java.io.Serializable;
  * @author Marcos Inacio de Paula Lima
  *
  */
-public abstract class Usuario implements Serializable {
+public abstract class Usuario implements Comparable<Usuario>, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String nome;
 	private char sexo;
 	private String cpf;
@@ -103,5 +107,53 @@ public abstract class Usuario implements Serializable {
 		this.dataNascimento = dataNascimento;
 	}
 	
-	
+	@Override
+	public int compareTo(Usuario usuario){
+		return this.getNome().compareTo(usuario.getNome());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((identidade == null) ? 0 : identidade.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj){
+			return true;
+		}	
+		if (obj == null){
+			return false;
+		}	
+		if (getClass() != obj.getClass()){
+			return false;
+		}	
+		Usuario other = (Usuario) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf)){
+			return false;
+	    }
+		if (identidade == null) {
+			if (other.identidade != null)
+				return false;
+		} else if (!identidade.equals(other.identidade)){
+			return false;
+		}	
+		if (nome == null) {
+			if (other.nome != null){
+				return false;
+			}	
+		} else if (!nome.equals(other.nome)){
+			return false;
+		}	
+		return true;
+	}
+		
 }
