d45021c85372f40650607c8c2a4c85760606dc82
Tamires Pereira <tamirespereira6@gmail.com>
2016-06-14 11:05:22 -0300

    oi

--- src/gui/TelaPrincipalNaoADM.java
+++ src/gui/TelaPrincipalNaoADM.java
@@ -9,12 +9,12 @@ import javax.swing.border.EmptyBorder;
 
 public class TelaPrincipalNaoADM extends JFrame {
 
-	private JPanel contentPane ;
+	private JPanel contentPane; 
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
