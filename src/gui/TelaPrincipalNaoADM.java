44b78e8a30a344020a7b363faff6b2f2fcb80fac
Tamires Pereira <tamirespereira6@gmail.com>
2016-06-14 11:02:34 -0300

    Tela principal nao adm

--- src/gui/TelaPrincipalNaoADM.java
+++ src/gui/TelaPrincipalNaoADM.java
@@ -9,7 +9,7 @@ import javax.swing.border.EmptyBorder;
 
 public class TelaPrincipalNaoADM extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel contentPane ;
 
 	/**
 	 * Launch the application.
