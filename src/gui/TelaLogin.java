17a447f6d57ae37bb0c9c382511004573153e8c6
ramices <ramicesmoises@gmail.com>
2016-06-14 22:11:41 -0300

    Main retirado

--- src/gui/TelaLogin.java
+++ src/gui/TelaLogin.java
@@ -43,22 +43,6 @@ public class TelaLogin extends JFrame {
     private TelaPrincipalNaoADM telaPrincipalNaoADM;
     private static Fachada fachada = Fachada.getInstance();
 	
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaLogin frame = new TelaLogin();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 
 	public TelaLogin() {
